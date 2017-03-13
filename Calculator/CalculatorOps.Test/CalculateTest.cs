947e8f9158c9242947d82dd3db02d1ab2172be34
Jaun Lloyd <jaunl@infoslips.com>
2017-03-13 07:47:03 +0200

    Added Files to GitHub

---
+++ Calculator/CalculatorOps.Test/CalculateTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CalculatorOps.Test
+{
+	[TestClass]
+	public class CalculateTest
+	{
+		[TestMethod]
+		public void ShouldReturnSeven()
+		{
+			Calculate cal = new Calculate();
+			int result = cal.Sum(4, 3);
+			Assert.AreEqual(7, result);
+		}
+		[TestMethod]
+		public void ShouldReturnTwelve()
+		{
+			Calculate cal = new Calculate();
+			int result = cal.Multiply(4, 3);
+			Assert.AreEqual(12, result);
+		}
+	}
+}
---
+++ Calculator/CalculatorOps/calculate.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorOps
+{
+	public class Calculate
+	{
+		public int Sum(int num1, int num2)
+		{
+			int result = num1 + num2;
+			return result;
+		}
+		public int Multiply(int num1, int num2)
+		{
+			int result = num1 + num2;
+			return result;
+		}
+	}
+}
